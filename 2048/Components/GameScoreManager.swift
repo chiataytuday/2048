2701d33e1c55e284755a4dd9f9217f5a8bff0ae7
Per Thoresson <per@thoresson.com.au>
2017-10-25 14:58:41 +0400

    Adding AdMob to AppDelegate

--- 2048/Components/GameScoreManager.swift
+++ 2048/Components/GameScoreManager.swift
@@ -9,32 +9,27 @@
 import Foundation
 
 class GameScoreManager {
-    
     static let sharedInstance = GameScoreManager()
     
     let highscore:String = "highscore"
-    
     let userDefaults = UserDefaults.standard
     
-    
     func saveScore(score:Int){
-        var scores = userDefaults.object(forKey: highscore) as? [Int] ?? [Int]()    // get current existing score
-        scores.append(score)                                                        // add score to stack
-        scores = scores.sorted(by: >)                                               // sort array decending
-        while scores.count > 10 { scores.removeLast() }                             // limit the highscore list to the last 10 games
-        userDefaults.set(scores, forKey: highscore)                                 // store the list of scores back to userdefaults
+        var scores = userDefaults.object(forKey: highscore) as? [Int] ?? [Int]()                            // get current existing score
+        scores.append(score)                                                                                // add score to stack
+        scores = scores.sorted(by: >)                                                                       // sort array decending
+        while scores.count > 10 { scores.removeLast() }                                                     // limit the highscore list to the last 10 games
+        userDefaults.set(scores, forKey: highscore)                                                         // store the list of scores back to userdefaults
     }
     
-    func getScores() -> Array<Int> {
-        return userDefaults.object(forKey: highscore) as? [Int] ?? [Int]()
-    }
+    func getScores() -> Array<Int> { return userDefaults.object(forKey: highscore) as? [Int] ?? [Int]() }   // Returns list of scores
     
-    func getHighScore() -> Int {
+    func getHighScore() -> Int {                                                                            // Return Highest Score
         var list = userDefaults.object(forKey: highscore) as? [Int] ?? [Int]()
         return list[0]
     }
     
     // Bool helpers
-    func getFlagForKey(key:String) -> Bool { return userDefaults.bool(forKey:key) }
-    func setFlagForKey(key:String, val:Bool){ userDefaults.set(val, forKey: key) }
+    func getFlagForKey(key:String) -> Bool { return userDefaults.bool(forKey:key) }                         // Returns Bool value for Key
+    func setFlagForKey(key:String, val:Bool){ userDefaults.set(val, forKey: key) }                          // Set Bool flag for Key
 }
