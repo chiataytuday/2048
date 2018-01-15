47513a816861a5207b635e047142520fdc00168b
Per Thoresson <perthoresson76@gmail.com>
2018-01-15 23:42:30 +0400

    Started howto Scene

--- 2048/InfoScene.swift
+++ 2048/InfoScene.swift
@@ -20,6 +20,7 @@ class InfoScene: SKScene {
     var lightNode:SCNNode! = nil
     var lockNode:SCNNode! = nil
     
+    var backPanel:SCNNode! = nil
     
     
     var gameViewController : GameViewController!
@@ -32,6 +33,7 @@ class InfoScene: SKScene {
         print("InfoScene - didMove")
         setupBg()                           //  background addition
         addStructure()                      // Add prerequisites
+        addContent()
         animateIn()
     }
     
@@ -79,10 +81,164 @@ class InfoScene: SKScene {
     }
 
     
-    func animateIn(){
+    func addContent(){
+        endpanelBgMat.diffuse.contents = col2048
+        let panelGeometry = SCNBox(width: panelWidth, height: panelHeight, length: panelDepth, chamferRadius: panelRadius)  // Cube Anim
+        backPanel = SCNNode(geometry: panelGeometry)
+        backPanel.name = "endpanel"
+        backPanel.geometry?.materials = [endpanelBgMat]
+        backPanel.position = SCNVector3(x: 0, y: 0, z: 0)
+        backPanel.pivot = SCNMatrix4MakeRotation(0.785398, 0, 0, 0);
+        backPanel.scale = SCNVector3Make(2.5, 2.5, 2.5)
         
+        infoSCNScene.rootNode.addChildNode(backPanel)
+        
+        let headerTxt = SCNText(string: "HIGHSCORE", extrusionDepth: 8)
+        headerTxt.font = UIFont(name: "Hangar-Flat", size: 20)
+        let headerNode = SCNNode(geometry: headerTxt)
+        headerNode.name = "gameoverHeader"
+        headerNode.scale = SCNVector3Make(0.03, 0.03, 0.03)
+        headerNode.position = SCNVector3Make(0.0, 1.35, 0.55)
+        headerTxt.flatness = 0.1
+        headerTxt.chamferRadius = 0.1
+        var goMinVec = SCNVector3Zero
+        var goMaxVec = SCNVector3Zero
+        if headerNode.__getBoundingBoxMin(&goMinVec, max: &goMaxVec) {
+            let distance = SCNVector3(
+                x: goMaxVec.x - goMinVec.x,
+                y: goMaxVec.y - goMinVec.y,
+                z: goMaxVec.z - goMinVec.z)
+            headerNode.pivot = SCNMatrix4MakeTranslation(distance.x / 2, distance.y / 3, distance.z / 2)
+        }
+        headerTxt.firstMaterial!.diffuse.contents = UIColor.white
+        headerTxt.firstMaterial!.specular.contents = UIColor.white
+        
+        infoSCNScene.rootNode.addChildNode(headerNode)
+        
+        // Copy
+        // :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+        // :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+        
+        // add home button
+        let howToCopy = """
+                        gfdkgjb
+                        gdfgdfgd
+                        89bv9cg89f8 fg 8f g9f
+                        """
+        let howtoTxt = SCNText(string: howToCopy, extrusionDepth: 8)
+        howtoTxt.font = UIFont(name: "Ionicons", size: 20)
+        let howtoNode = SCNNode(geometry: howtoTxt)
+        howtoNode.name = "howto"
+        howtoNode.scale = SCNVector3Make(0.035, 0.035, 0.035)
+        howtoNode.position = SCNVector3Make(0.0, -1.65, 0.55)
+        howtoTxt.flatness = 0.1
+        howtoTxt.chamferRadius = 0.1
+        var htMinVec = SCNVector3Zero
+        var htMaxVec = SCNVector3Zero
+        if howtoNode.__getBoundingBoxMin(&htMinVec, max: &htMaxVec) {
+            let distance = SCNVector3(
+                x: htMaxVec.x - htMinVec.x,
+                y: htMaxVec.y - htMinVec.y,
+                z: htMaxVec.z - htMinVec.z)
+            howtoNode.pivot = SCNMatrix4MakeTranslation(distance.x / 2, distance.y / 3, distance.z / 2)
+        }
+        howtoTxt.firstMaterial!.diffuse.contents = UIColor.white
+        howtoTxt.firstMaterial!.specular.contents = UIColor.white
+        infoSCNScene.rootNode.addChildNode(howtoNode)
+        // :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+        // :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+        
+        
+        // add home button
+        let homeBtnTxt = SCNText(string: "\u{f38f}", extrusionDepth: 8)
+        homeBtnTxt.font = UIFont(name: "Ionicons", size: 20)
+        let homeBtnNode = SCNNode(geometry: homeBtnTxt)
+        homeBtnNode.name = "homeBtn"
+        homeBtnNode.scale = SCNVector3Make(0.035, 0.035, 0.035)
+        homeBtnNode.position = SCNVector3Make(0.0, -1.65, 0.55)
+        homeBtnTxt.flatness = 0.1
+        homeBtnTxt.chamferRadius = 0.1
+        var hbMinVec = SCNVector3Zero
+        var hbMaxVec = SCNVector3Zero
+        if homeBtnNode.__getBoundingBoxMin(&hbMinVec, max: &hbMaxVec) {
+            let distance = SCNVector3(
+                x: hbMaxVec.x - hbMinVec.x,
+                y: hbMaxVec.y - hbMinVec.y,
+                z: hbMaxVec.z - hbMinVec.z)
+            homeBtnNode.pivot = SCNMatrix4MakeTranslation(distance.x / 2, distance.y / 3, distance.z / 2)
+        }
+        homeBtnTxt.firstMaterial!.diffuse.contents = UIColor.white
+        homeBtnTxt.firstMaterial!.specular.contents = UIColor.white
+        
+        infoSCNScene.rootNode.addChildNode(homeBtnNode)
     }
     
+    
+    
+    func animateIn(){
+        SCNTransaction.begin()
+        SCNTransaction.animationDuration = 1.0
+        cameraNode.position = highscoreCameraIn
+        light.intensity = 1200
+        SCNTransaction.commit()
+    }
+    
+    func animateOut(){
+        SCNTransaction.begin()
+        SCNTransaction.animationDuration = 1.0
+        cameraNode.position = highscoreCameraOut
+        light.intensity = 0
+        SCNTransaction.commit()
+    }
+    
+    // Touch handlers
+    func touchDown(atPoint pos : CGPoint) {}
+    func touchMoved(toPoint pos : CGPoint) {}
+    func touchUp(atPoint pos : CGPoint) {}
+    
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
+        
+        let touch = touches.first
+        if let touchPoint = touch?.location(in: self.infoView),
+            let hitTestResult = self.infoView.hitTest(touchPoint, options: nil).first {
+            let hitNode = hitTestResult.node
+            print("Name : ",hitNode.name as Any)
+            var exit:CGFloat? = nil
+            if hitNode.name == "homeBtn" { exit = scenes.home }
+            if exit != nil {
+                exitToScene(scene: exit!)
+            }
+        }
+    }
+    
+    
+    // Transition to scenes
+    // ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+    func exitToScene(scene:CGFloat){
+        run(SKAction.sequence([
+            SKAction.run() {
+                self.cameraNode.constraints = nil
+                self.removeSceneAnim()
+            },
+            SKAction.wait(forDuration: 1.0),
+            SKAction.run() {
+                self.gameViewController.moveToScene(to: scene)
+            }
+            ]), withKey:"transitioning")
+    }
+    
+    func removeSceneAnim(){
+        // animate cube out
+        SCNTransaction.begin()
+        SCNTransaction.animationDuration = 1.0
+        cameraNode.position = highscoreCameraOut
+        light.intensity = 0
+        SCNTransaction.commit()
+        // animate logo out
+    }
     
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
